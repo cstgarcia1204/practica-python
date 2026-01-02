5d6f629bad387e32f327fc9c13596a5b125b55a0
cstg <ckstgarcia@gmail.com>
2026-01-02 16:50:45 -0700

    la parte de print

---
+++ 01_print.py
@@ -0,0 +1,17 @@
+#el metodo o funcion o modulo para imprimir en python
+print("Hola mundo desde mi cursito")
+
+#cadenas de texto con una comilla cuando se puede utilizar una u otra
+# dependiendo de lo que se quiera imprimir
+print('esto tambien funciona pue\' ')
+#por defecto el separador es espacio en blanco con la coma
+print("Esto","es ","mi"," nombre", " Cithia")
+#El separador de python es configurable no solo el espacio en blanco sino que le dices que separador quieres usar
+print("Python", "es", "chido", sep="_")
+#tambien existe el end que por defecto renderiza un salto de linea pero tambien se puede personalizar
+print("esto es una linea checa", end="@")
+print("otra linea")
+#tambien se imprimen numeros
+print(334)
+
+
