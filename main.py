36fb4be0b7dbc4c23220521370f1e3ea27274da6
cstg <ckstgarcia@gmail.com>
2026-01-02 17:07:46 -0700

    tipos de datos

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
 #Curso
+#Tipos de datos int, float, complex, str, bool, NoneType, list, tuple, dict, range, set...
 
+#funcion type imprime en consola cual es el tipo valor de dato
+print('int:')
+print(type(10))
+print(type(0))
+print(type(-5))
+print(type(2039520958475))
+#tambien en python la parte de los numeros son muy exactos
+print(3452435345+1)
+'''
+Estamos haciendo esto como una cadena de texto para documentar no para comentario
+'''
+#float
+print(type(3.12))
+print(type(1.0))
+print(type(1e3))
+
+#numeros complejos
+print("complejos o complex:")
+print(type(1+2j))
+
+print("str string:")
+print(type("hola"))
+print(type(""))
+print(type("123"))
+print(type("""
+    Multilinea
+"""))
+
+#booleanos
+print("bool:")
+print(type(True))
+print(type(False))
+print(type(1<2))
+
+#ausencia de valor, se usa para la ausencia de valor como el null pero en js es diferente / es entre el null y undefine
+print(type("NoneType:"))
+print(type(None))
