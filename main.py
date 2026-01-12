49a755449447624bf26dce7d21c082c118687b4a
cstg <ckstgarcia@gmail.com>
2026-01-12 15:33:24 -0700

    agregando el challenge de Jurassic Park

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
 #Curso
 """
-Esta en Equilibrio la Alianza entre Reed Richards y Jhonny Storm?
-En el universo de los 4 Fantasticos, la union y el equilibrio entre los poderes es fundamental para enfrentar
-cualquier desafio. Rn este problema, nos centraremos en dos de sus miembros:
+En Jurassic Park, se ha observado que los dinosaurios carnívoros, como el temible T'Rex, depositan un número par
+de huevos. Imagina que tienes una lista de números enteros en la que cada número representa la cantidad de
+huevos puestos por un dinosaurio en el parque.
 
-Reed Richards (Mr. Fantastic), representado por la letra R.
-Jhonny Storm (La Antorcha Humana), representado por la letra J.
+Importante: Solo se consideran los huevos de los dinosaurios carnívoros (T-Rex) aquellos números que son pares.
 
 Objetivo:
-Crea una funcion en Python que reciba una cadena de texto.
-Esta funcion debe contar cuantas veces aparece la letra R (para Reed Richards) y
-cuantas veces aparece la letra J (para Johnny Storm) en la cadena.
-
- - Si la cantidad de R y la cantidad de J son iguales, se considera que la alianza
- entre la mente y el fuego esta en equilibrio y la funcion debe retornar True
-
- - Si las cantidades no son iguales, la funcion debe retornar False.
-
- - En el caso de que no aparezca ninguna de las dos letras en la cadena,
- se entiende que el equilibrio se mantiene (0=0), por lo que la funcion debe retornar True.
+Escribne una función en Python que reciba una lista de números enteros y devielva la suma total de los huevos que
+pertenecen a los dinosaurios carnívoros (es decir la suma de todos los números pares en la lista).
 """
-# usar diccionarios
-def check_is_balanced(text):
-    #transformarlo todo el texto en mayusculas
-    text = text.upper()
-    #contar facilmente el numero de veces que aparece una letra
-    count_r = text.count("R")  #Reed Richards
-    count_j = text.count("J")  #Johnny Storm
+"""
+Esta es mi solucion propia pero vamos a recrear la del curso midu
+"""
+def num_huevos(huevos):
+    suma = 0
+    for huevo in huevos:
+        if huevo % 2 == 0:
+            suma += huevo
+    return suma
 
-    print(f"count_r: {count_r} count_j: {count_j}")
-    # if count_r == count_j:
-    #     return True
-    # else:
-    #     return False
-    #esa condicion con if se puede mejorar porque es muy verbosa entonces directamente retorna la comparacion como se muestra a continuacion:
-    return count_r == count_j
+print(num_huevos([1, 2, 3, 4, 6, 8, 10])) #30
 
 
+def count_carnivore_dinosaur_eggs(egg_list) -> int: #Estamos definiendo el tipo de dato que devuelve es un int
+    """
+    Esta funcion recibe una lista de numeros enteros que representan la cantidad de huevos que han puesto diferentes
+    dinosaurios ene l parque jurasico y los de numero par son de carnivoros. Devuelve un numero con la suma
+    de todos los huevos de carnivoros.
+    """
+    total_carnivore_eggs = 0
 
-
-
-print(check_is_balanced("cinthia"))
+    for eggs in egg_list:
+        if eggs % 2 == 0:
+            total_carnivore_eggs += eggs
+    return total_carnivore_eggs
+egg_list = [3, 4, 7, 5, 8]
+print(count_carnivore_dinosaur_eggs(egg_list))
 
